7fe94845415c385b8db3a94e884ddcb860091f67
ilhampratama2109 <pilham902@gmail.com>
2022-04-04 04:26:40 +0700

    FIX

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # main program yang digunakan untuk menjalankan program
 
+from asyncio.windows_events import NULL
 from FileHandler import *
 from Puzzle import *
 import time
@@ -7,45 +8,44 @@ import copy
 
 
 print("==== Puzzle Solver ====")
-dir = input("masukkan nama file: ")
+directory = input("masukkan nama file: ")
 filePath = "E:/semester 4/strategi algoritma/tucil/Tucil 3/Tucil3_IF2211_Strategi_Algoritma/src/"
-# try:
+
 # membaca input file
 file = Filehandler()
-puzzle15 = file.bacaFile(filePath + dir)
+puzzle15 = file.bacaFile(filePath + directory)
 
+try:
 # waktu dimulai
-start = time.time()
-print("==== Puzzle =====")
-puzzle15.printmatrik()
-print("==== Kurang(i) ====")
+    start = time.time()
+    print("==== Puzzle =====")
+    puzzle15.printMatrik()
+    print("==== Kurang(i) ====")
 
-if(puzzle15.isSolveable()):
-    puzzle15.printInverse()
-    print("Puzzle bisa diselesaikan")
-    print("================")
-    print("= Penyelesaian =")
-    temp = copy.deepcopy(puzzle15)
-    puzzle15.container[tuple(puzzle15.convertToArray(puzzle15.matrik))] = 'none'
-    step = 0
-    while (not puzzle15.isSolution()):
-        puzzle15.solve()
-        step += 1
+    if(puzzle15.isSolveable()):
+        puzzle15.printInverse()
+        print("Puzzle bisa diselesaikan")
+        print("================")
+        print("= Penyelesaian =")
+        temp = copy.deepcopy(puzzle15)
+        puzzle15.container[tuple(puzzle15.convertToArray(puzzle15.matrik))] = 'none'
+        step = 0
+        while (not puzzle15.isSolution()):
+            puzzle15.solve()
+            step += 1
 
-    temp.path = puzzle15.path.split(" ")
-    while (len(temp.path) != 1):
-        temp.evalPath()
-        temp.printmatrik()
-        print("=================")
-    print("==== Selesai ====")
-    print("Jumlah simpul dibangkitkan : " +
-          str(len(puzzle15.container) - 1))
-else:
-    puzzle15.printInverse()
-    print("Puzzle tidak bida diselesaikan")
-    print("==============================")
-end = time.time()
-print("Time : " + str(end-start) + " detik")
-
-# except:
-#     print("File tidak ditemukan")
+        temp.path = puzzle15.path.split(" ")
+        while (len(temp.path) != 1):
+            temp.pathEvaluation()
+            temp.printMatrik()
+            print("=================")
+        print("==== Selesai ====")
+        print("Jumlah simpul dibangkitkan : " + str(len(puzzle15.container) - 1))
+    else:
+        puzzle15.printInverse()
+        print("Puzzle tidak bida diselesaikan")
+        print("==============================")
+    end = time.time()
+    print("Time : " + str(end-start) + " detik")
+except:
+    print("File tidak ditemukan")
